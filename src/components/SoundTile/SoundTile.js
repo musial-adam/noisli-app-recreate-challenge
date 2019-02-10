89599ede6cc9327f0506a5f971710ca9f1d97de2
Adam Musiał <adam.musiall@gmail.com>
2019-02-10 23:36:57 +0100

    add keyPress event handling

--- src/components/SoundTile/SoundTile.js
+++ src/components/SoundTile/SoundTile.js
@@ -4,6 +4,8 @@ import styles from './SoundTile.module.scss';
 
 import Slider from '../Slider/Slider';
 
+//! Work on <track> for audio files!
+
 export class SoundTile extends Component {
   static propTypes = {
     icon: PropTypes.string.isRequired,
@@ -15,47 +17,67 @@ export class SoundTile extends Component {
     on: false,
   };
 
+  // componentDidMount() {
+  //   document.addEventListener('keypress', this.handleKeyPress);
+  // }
+
+  // componentWillUnmount() {
+  //   document.removeEventListener('keypress', this.handleKeyPress);
+  // }
+
   toggleHandler = () => {
     this.togglePlay();
-    this.setState((state, props) => {
+    this.setState(state => {
       return { on: !state.on };
     });
   };
 
+  handleKeyPress = event => {
+    if (event.key === ' ') {
+      event.preventDefault();
+      this.toggleHandler();
+    }
+  };
+
   togglePlay = () => {
-    const sound = document.getElementById(this.props.sound);
-    if (this.state.on) {
-      sound.pause();
+    const { on } = this.state;
+    const { sound } = this.props;
+
+    const soundHook = document.getElementById(sound);
+    if (on) {
+      soundHook.pause();
     } else {
-      sound.currentTime = 0;
-      sound.play();
+      soundHook.currentTime = 0;
+      soundHook.play();
     }
   };
 
   render() {
+    const { on } = this.state;
+    const { alt, icon, sound } = this.props;
     return (
-      <div className={styles.SoundTile} onClick={this.toggleHandler}>
-        {this.state.on ? (
+      <div
+        className={styles.SoundTile}
+        onClick={this.toggleHandler}
+        onKeyPress={this.handleKeyPress}
+        role="button"
+        tabIndex={0}
+      >
+        {on ? (
           <>
-            <img
-              src={`./assets/icons/${this.props.icon}`}
-              alt={this.props.alt}
-            />
+            <img src={`./assets/icons/${icon}`} alt={alt} />
             <Slider />
           </>
         ) : (
           <img
-            src={`./assets/icons/${this.props.icon}`}
-            alt={this.props.alt}
+            src={`./assets/icons/${icon}`}
+            alt={alt}
             style={{ opacity: 0.5 }}
           />
         )}
-        <audio id={this.props.sound} preload="auto" loop>
-          {/* <audio id={this.props.sound} preload="auto" loop controls> */}
-          <source
-            src={`./assets/sounds/${this.props.sound}`}
-            type="audio/mp4"
-          />
+        <audio id={sound} preload="auto" loop>
+          <track default kind="captions" />
+          <source src={`./assets/sounds/${sound}`} type="audio/mp4" />
           Your browser does not support the <code>audio</code> element.
         </audio>
       </div>
